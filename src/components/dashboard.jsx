bbd5ae1aaa5e24785ec425ac281f65a92b8d01d1
toruthecoder <devishsage@gmail.com>
2026-02-18 21:36:34 +0500

    push

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -11,11 +11,15 @@ import { CiDollar } from "react-icons/ci";
 const Dashboard = () => {
 
     const [data, setData] = useState(null)
+    const [odata, setOdata] = useState(null);
+
     useEffect(() => {
+        // Function for getting all the data
         const fetchData = async () => {
             try {
                 const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/store/data`)
                 setData(response.data)
+                console.log(response.data)
             } catch (error) {
                 console.error(error)
             }
@@ -23,37 +27,103 @@ const Dashboard = () => {
         fetchData()
     }, [])
 
+    useEffect(() => {
+        // Function for getting the Order data
+        const getOrderData = async () => {
+            try {
+                const res = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/store/orders`)
+                console.log(res.data)
+                setOdata(res.data)
+            } catch (error) {
+                console.error(error)
+            }
+        }
+        getOrderData()
+    }, [])
+
     return (
-        <div className='mt-4 flex gap-8 flex-row'>
+        <div className='flex flex-col'>
+            <div className='mt-4 flex gap-6 flex-row'>
+                <div className='flex flex-col'>
 
-            <div className='flex flex-col gap-6'>
-                <h1 className="text-[20px] font-bold">Inventory</h1>
-                <div>
-                    <div className='bg-white p-8 flex flex-col justify-center items-left rounded-4xl pl-4 w-[230px]'>
+                    <div>
+                        <div className='bg-white p-8 flex flex-col justify-center items-left rounded-4xl pl-4 w-[250px]'>
+                            <div className='flex gap-4 items-center'>
+                                <div className='bg-[#e7e7e7] p-2 rounded-4xl'>
+                                    <CiShoppingBasket size={35} />
+                                </div>
+                                <h1 className='text-[18px]'>total orders</h1>
+                            </div>
+                            <div className='text-[41px] ml-2'>
+                                <h1>{data?.stats?.totalOrders}</h1>
+                            </div>
+                        </div>
+
+                        <div className='bg-white p-8 flex flex-col justify-center items-left rounded-4xl pl-4 w-[250px] mt-6'>
+                            <div className='flex gap-4 items-center'>
+                                <div className='bg-[#e7e7e7] p-2 rounded-4xl'>
+                                    <PiArrowCounterClockwiseLight size={35} />
+                                </div>
+                                <h1 className='text-[18px]'>total Refunds</h1>
+                            </div>
+                            <div className='text-[41px] ml-2'>
+                                <h1>{data?.stats?.totalReturns}</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='grid grid-cols-4 gap-6 w-full'>
+
+                    <div className='bg-white p-8 flex flex-col items-left rounded-4xl pl-4 w-[250px] h-[178px]'>
                         <div className='flex gap-4 items-center'>
                             <div className='bg-[#e7e7e7] p-2 rounded-4xl'>
-                                <CiShoppingBasket size={35} />
+                                <CiShoppingCart size={35} />
                             </div>
-                            <h1 className='text-[18px]'>total orders</h1>
+                            <h1 className='text-[18px]'>Total Sales</h1>
                         </div>
                         <div className='text-[41px] ml-2'>
-                            <h1>{data?.stats?.totalOrders}</h1>
+                            {Math.floor(data?.stats?.salesResult)}
                         </div>
                     </div>
 
-                    <div className='bg-white p-8 flex flex-col justify-center items-left rounded-4xl pl-4 w-[230px] mt-6'>
+                    <div className='bg-white p-8 flex flex-col items-left rounded-4xl pl-4 w-[250px] h-[178px]'>
                         <div className='flex gap-4 items-center'>
                             <div className='bg-[#e7e7e7] p-2 rounded-4xl'>
-                                <PiArrowCounterClockwiseLight size={35} />
+                                <PiUsersThreeLight size={35} />
                             </div>
-                            <h1 className='text-[18px]'>total Refunds</h1>
+                            <h1 className='text-[18px]'>Total Customers</h1>
                         </div>
                         <div className='text-[41px] ml-2'>
-                            <h1>{data?.stats?.totalReturns}</h1>
+                            {data?.stats?.totalCustomer}
                         </div>
                     </div>
 
-                    <div className='bg-white p-8 flex flex-col justify-center items-left rounded-4xl pl-4 w-[230px] mt-6'>
+                    <div className='bg-white p-8 flex flex-col items-left rounded-4xl pl-4 w-[250px] h-[178px]'>
+                        <div className='flex gap-4 items-center'>
+                            <div className='bg-[#e7e7e7] p-2 rounded-4xl'>
+                                <PiMoneyWavyLight size={35} />
+                            </div>
+                            <h1 className='text-[18px]'>Total Revenue</h1>
+                        </div>
+                        <div className='text-[41px] ml-2'>
+                            {Math.floor(data?.stats?.totalRevenue)} Rs
+                        </div>
+                    </div>
+
+                    <div className='bg-white p-8 flex flex-col items-left rounded-4xl pl-4 w-[250px] h-[178px]'>
+                        <div className='flex gap-4 items-center'>
+                            <div className='bg-[#e7e7e7] p-2 rounded-4xl'>
+                                <CiDollar size={35} />
+                            </div>
+                            <h1 className='text-[18px]'>Total Costs</h1>
+                        </div>
+                        <div className='text-[41px] ml-2'>
+                            {Math.floor(data?.stats?.totalCosts)}
+                        </div>
+                    </div>
+
+                    <div className='bg-white p-8 flex flex-col justify-center items-left rounded-4xl pl-4 w-[250px]'>
                         <div className='flex gap-4 items-center'>
                             <div className='bg-[#e7e7e7] p-2 rounded-4xl'>
                                 <PiPackageThin size={35} />
@@ -64,81 +134,95 @@ const Dashboard = () => {
                             <h1>{data?.stats?.totalProducts}</h1>
                         </div>
                     </div>
+
+                    <div className='bg-white p-8 flex flex-col items-left rounded-4xl pl-4 w-[250px] h-[178px] '>
+                        <div className='flex gap-4 items-center'>
+                            <div className='bg-[#e7e7e7] p-2 rounded-4xl'>
+                                <CiDollar size={35} />
+                            </div>
+                            <h1 className='text-[18px]'>Net Profit</h1>
+                        </div>
+                        <div className='text-[41px] ml-2'>
+                            {Math.floor(data?.stats?.netProfit)}
+                        </div>
+                    </div>
+
+                    <div className='bg-white p-8 flex flex-col items-left rounded-4xl pl-4 w-[250px] h-[178px] '>
+                        <div className='flex gap-4 items-center'>
+                            <div className='bg-[#e7e7e7] p-2 rounded-4xl'>
+                                <CiDollar size={35} />
+                            </div>
+                            <h1 className='text-[18px]'>Profit Margin</h1>
+                        </div>
+                        <div className='text-[41px] ml-2'>
+                            {Math.floor(data?.stats?.profitMargin)}
+                        </div>
+                    </div>
                 </div>
+
             </div>
 
-            <div className='grid grid-cols-4 gap-6 w-full mt-[52px]'>
-
-                <div className='bg-white p-8 flex flex-col items-left rounded-4xl pl-4 w-[250px] h-[178px]'>
-                    <div className='flex gap-4 items-center'>
-                        <div className='bg-[#e7e7e7] p-2 rounded-4xl'>
-                            <CiShoppingCart size={35} />
-                        </div>
-                        <h1 className='text-[18px]'>Total Sales</h1>
-                    </div>
-                    <div className='text-[41px] ml-2'>
-                        {Math.floor(data?.stats?.salesResult)}
-                    </div>
-                </div>
-
-                <div className='bg-white p-8 flex flex-col items-left rounded-4xl pl-4 w-[250px] h-[178px]'>
-                    <div className='flex gap-4 items-center'>
-                        <div className='bg-[#e7e7e7] p-2 rounded-4xl'>
-                            <PiUsersThreeLight size={35} />
-                        </div>
-                        <h1 className='text-[18px]'>Total Customers</h1>
-                    </div>
-                    <div className='text-[41px] ml-2'>
-                        {data?.stats?.totalCustomer}
-                    </div>
-                </div>
-
-                <div className='bg-white p-8 flex flex-col items-left rounded-4xl pl-4 w-[250px] h-[178px]'>
-                    <div className='flex gap-4 items-center'>
-                        <div className='bg-[#e7e7e7] p-2 rounded-4xl'>
-                            <PiMoneyWavyLight size={35} />
-                        </div>
-                        <h1 className='text-[18px]'>Total Revenue</h1>
-                    </div>
-                    <div className='text-[41px] ml-2'>
-                        {Math.floor(data?.stats?.totalRevenue)} Rs
-                    </div>
-                </div>
-
-                <div className='bg-white p-8 flex flex-col items-left rounded-4xl pl-4 w-[250px] h-[178px]'>
-                    <div className='flex gap-4 items-center'>
-                        <div className='bg-[#e7e7e7] p-2 rounded-4xl'>
-                            <CiDollar size={35} />
-                        </div>
-                        <h1 className='text-[18px]'>Total Costs</h1>
-                    </div>
-                    <div className='text-[41px] ml-2'>
-                        {Math.floor(data?.stats?.totalCosts)}
-                    </div>
-                </div>
-
-                <div className='bg-white p-8 flex flex-col items-left rounded-4xl pl-4 w-[250px] h-[178px] -mt-47'>
-                    <div className='flex gap-4 items-center'>
-                        <div className='bg-[#e7e7e7] p-2 rounded-4xl'>
-                            <CiDollar size={35} />
-                        </div>
-                        <h1 className='text-[18px]'>Net Profit</h1>
-                    </div>
-                    <div className='text-[41px] ml-2'>
-                        {Math.floor(data?.stats?.netProfit)}
-                    </div>
-                </div>
-
-                <div className='bg-white p-8 flex flex-col items-left rounded-4xl pl-4 w-[250px] h-[178px] -mt-47'>
-                    <div className='flex gap-4 items-center'>
-                        <div className='bg-[#e7e7e7] p-2 rounded-4xl'>
-                            <CiDollar size={35} />
-                        </div>
-                        <h1 className='text-[18px]'>Profit Margin</h1>
-                    </div>
-                    <div className='text-[41px] ml-2'>
-                        {Math.floor(data?.stats?.profitMargin)}
-                    </div>
+            <div className="order flex items-center justify-center mt-10">
+                <div className='pt-10 bg-white p-10'>
+                    <table className='pt-10'>
+                        <tbody className='bg-white'>
+                            <tr className='bg-[#e7e7e7] text-gray-500'>
+                                <th>Order ID</th>
+                                <th>Date</th>
+                                <th>Revenue</th>
+                                <th>Total Cost</th>
+                                <th>Profit</th>
+                                <th>Status</th>
+                            </tr>
+                            <tr className=''>
+                                <td className='p-4'>
+                                    {odata?.order?.map((order, index) => (
+                                        <div key={index} className='p-2'>{order.orderId}</div>
+                                    ))}
+                                </td>
+                                <td className='p-4'>
+                                    {odata?.order?.map((order, index) => (
+                                        <div key={index} className='p-2'>
+                                            {new Date(order?.rawData?.updated_at).toLocaleString()}
+                                        </div>
+                                    ))}
+                                </td>
+                                <td className='p-4'>
+                                    {
+                                        odata?.order?.map((order, index) => (
+                                            <div key={index} className='p-2'>
+                                                {Math.round(order?.totalPrice - order?.rawData?.total_discounts - order?.rawData?.total_cash_rounding_refund_adjustment_set?.presentment_money?.amount)} rs
+                                            </div>
+                                        ))
+                                    }
+                                </td>
+                                <td className='p-4'>
+                                    {
+                                        odata?.order?.map((order, index) => (
+                                            <div key={index} className='p-2'>
+                                                {Math.round(order?.totalPrice) + Math.round(order?.rawData?.total_shipping_price_set?.presentment_money?.amount) +
+                                                    Math.round(order?.rawData?.total_cash_rounding_refund_adjustment_set?.presentment_money?.amount)} rs
+                                            </div>
+                                        ))
+                                    }
+                                </td>
+                                <td className='p-4'>
+                                    {data?.singleNetProfit?.map((order, index) => (
+                                        <div key={index} className='p-2'>
+                                            {order?.netProfit} rs
+                                        </div>
+                                    ))}
+                                </td>
+                                <td className='p-6'>
+                                    {data?.singleNetProfit?.map((order, index) => (
+                                        <div key={index} className='p-2'>
+                                            {order?.netProfit > 0 ? <div className='text-green-600 font-semibold'>profit</div> : <div className='text-red-600 font-semibold'>loss</div>}
+                                        </div>
+                                    ))}
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,19 +1,31 @@
 import { Link, Outlet } from 'react-router-dom'
+import { TbLayoutDashboard } from "react-icons/tb";
+import { LuClipboardList } from "react-icons/lu";
+import { useState } from "react";
 
 function Nav() {
+    const [isExpanded, setIsExpanded] = useState(false);
     return (
         <>
-            <nav className='w-full bg-[#eee]'>
-                <ul className='flex gap-5 items-center justify-center'>
+            <nav className='w-[40px] h-180 top-38 left-0 bg-white absolute rounded-2xl'
+                onMouseEnter={() => setIsExpanded(true)}
+                onMouseLeave={() => setIsExpanded(false)}
+                style={{
+                    width: isExpanded ? "150px" : "40px",
+                    color: isExpanded ? "#343434" : "black",
+                    transition: "width 0.3s ease",
+                    overflow: "hidden",
+                }}>
+                <ul className='flex flex-col gap-5 items-left justify-left pl-2 pt-4'>
                     <li>
-                        <Link to='/'>Dashboard</Link>
+                        <Link to='/' className='flex gap-2'><TbLayoutDashboard size={25} /> {isExpanded === true ? 'DashBoard' : null}</Link>
                     </li>
 
                     <li>
-                        <Link to='/rawdata'>Raw Data</Link>
+                        <Link to='/rawdata' className='flex gap-2'><LuClipboardList size={25} />{isExpanded === true ? 'Products' : null}</Link>
                     </li>
                 </ul>
-            </nav>
+            </nav >
             <Outlet />
         </>
     )
